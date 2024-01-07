c48d75e17842cac83ddf80fc790fff6cb30f546c
Julie Engel <julie.engel@mail.com>
2024-01-07 23:49:23 +0100

    uppercase vars, add more echo statements, shellcheck scripts

--- 01_create_instances.sh
+++ 01_create_instances.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 # Define variables
-vpc_name="jitsi-vpc"
-vpc_cidr="10.0.0.0/16" # CIDR block for VPC
-instance_type="t2.micro"
-ami="ami-0faab6bdbac9486fb"  # Ubuntu in eu-central-1
-key_name="jitsi-central-luca" # TODO use generated keys
-user_data_script="jitsi_setup.sh"
-security_group_name="jitsi-security-group"
-auto_scaling_group_name="jitsi-auto-scaling-group"
-launch_template_name="jitsi-launch-template"
-target_group_name="jitsi-target"
+VPC_NAME="jitsi-vpc"
+SECURITY_GROUP_NAME="jitsi-security-group"
+TARGET_GROUP_NAME="jitsi-target"
+AUTOSCALING_GROUP_NAME="jitsi-auto-scaling-group"
+LAUNCH_TEMPLATE_NAME="jitsi-launch-template"
+
+# Params for EC2 setup
+EC2_USER_DATA_SCRIPT="resources/jitsi_setup.sh"
+EC2_AMI="EC2_AMI-0faab6bdbac9486fb"  # Ubuntu in eu-central-1
+EC2_INSTANCE_TYPE="t2.micro"
+EC2_KEY_NAME="jitsi-ec2-key"
 
 # Function to create a security group within the VPC
 create_security_group() {
@@ -23,24 +24,26 @@ create_security_group() {
     --vpc-id "$vpc_id" --output text
 }
 
+echo "Creating VPC with Internet Gateway and Security Group..."
+
 # Create VPC
-vpc_id=$(aws ec2 create-vpc --cidr-block "$vpc_cidr" --query 'Vpc.VpcId' --output text)
-aws ec2 create-tags --resources "$vpc_id" --tags "Key=Name,Value=$vpc_name"
+vpc_id=$(aws ec2 create-vpc --cidr-block "10.0.0.0/16" --query 'Vpc.VpcId' --output text)
+aws ec2 create-tags --resources "$vpc_id" --tags "Key=Name,Value=$VPC_NAME"
 
 # Create and attach Internet Gateway (IGW) to the VPC
 igw_id=$(aws ec2 create-internet-gateway --query 'InternetGateway.InternetGatewayId' --output text)
 aws ec2 attach-internet-gateway --internet-gateway-id "$igw_id" --vpc-id "$vpc_id"
 
 # Create security group for VPC
-security_group_id=$(create_security_group "$vpc_id" "$security_group_name")
+security_group_id=$(create_security_group "$vpc_id" "$SECURITY_GROUP_NAME")
 
 # Add inbound rules to the security group
 security_group_rules=("tcp/22" "tcp/80" "tcp/443" "tcp/4443" "udp/10000")
 for rule in "${security_group_rules[@]}"; do
   aws ec2 authorize-security-group-ingress \
     --group-id "$security_group_id" \
-    --protocol $(echo "$rule" | cut -d'/' -f1) \
-    --port $(echo "$rule" | cut -d'/' -f2) \
+    --protocol "$(echo "$rule" | cut -d'/' -f1)" \
+    --port "$(echo "$rule" | cut -d'/' -f2)" \
     --cidr 0.0.0.0/0
 done
 
@@ -72,32 +75,41 @@ aws ec2 create-route \
   --route-table-id "$main_route_table_id" \
   --destination-cidr-block 0.0.0.0/0 \
   --gateway-id "$igw_id"
-  
+
+echo "VPC with Internet Gateway and Security Group has been configured."
+
+echo "Creating Target Group..."
 
 # Create Target Group
 target_group_id=$(aws elbv2 create-target-group \
-  --name "$target_group_name" \
+  --name "$TARGET_GROUP_NAME" \
   --protocol HTTPS \
   --port 443 \
   --vpc-id "$vpc_id" \
   --query 'TargetGroups[0].TargetGroupArn' \
   --output text)
 
+echo "Target Group has been configured."
+
+echo "Creating Launch Template..."
 
 # Base64 encode the user data script without newlines
-user_data_base64=$(base64 -w 0 "$user_data_script")
-
+user_data_base64=$(base64 -w 0 "$EC2_USER_DATA_SCRIPT")
 
 # Create Launch Template for Auto Scaling Group
 aws ec2 create-launch-template \
-  --launch-template-name "$launch_template_name" \
+  --launch-template-name "$LAUNCH_TEMPLATE_NAME" \
   --version-description "Version1" \
-  --launch-template-data "{\"ImageId\":\"$ami\",\"InstanceType\":\"$instance_type\",\"KeyName\":\"$key_name\",\"UserData\":\"$user_data_base64\",\"NetworkInterfaces\":[{\"DeviceIndex\":0,\"AssociatePublicIpAddress\":true,\"Groups\":[\"$security_group_id\"]}]}"
+  --launch-template-data "{\"ImageId\":\"$EC2_AMI\",\"InstanceType\":\"$EC2_INSTANCE_TYPE\",\"KeyName\":\"$EC2_KEY_NAME\",\"UserData\":\"$user_data_base64\",\"NetworkInterfaces\":[{\"DeviceIndex\":0,\"AssociatePublicIpAddress\":true,\"Groups\":[\"$security_group_id\"]}]}"
+
+echo "Launch Template has been configured."
+
+echo "Creating Auto Scaling Group..."
 
 # Create Auto Scaling Group using Launch Template
 aws autoscaling create-auto-scaling-group \
-  --auto-scaling-group-name "$auto_scaling_group_name" \
-  --launch-template "LaunchTemplateName=$launch_template_name,Version=1" \
+  --auto-scaling-group-name "$AUTOSCALING_GROUP_NAME" \
+  --launch-template "LaunchTemplateName=$LAUNCH_TEMPLATE_NAME,Version=1" \
   --min-size 1 \
   --max-size 1 \
   --desired-capacity 1 \
--- 02_create_load_balancer.sh
+++ 02_create_load_balancer.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
 # Define variables
-vpc_name="jitsi-vpc"
-security_group_name="jitsi-security-group"
-load_balancer_name="jitsi-lb"
-certificate_domain="meet.xn--rotzlffel-47a.ch"
-target_group_name="jitsi-target" 
+VPC_NAME="jitsi-vpc"
+SECURITY_GROUP_NAME="jitsi-security-group"
+TARGET_GROUP_NAME="jitsi-target"
+LOAD_BALANCER_NAME="jitsi-lb"
+CERTIFICATE_DOMAIN_NAME="meet.xn--rotzlffel-47a.ch"
+
+echo "Creating ALB with existing VPC, Subnets and Security Group..."
 
 # Retrieve VPC ID
 vpc_id=$(aws ec2 describe-vpcs \
-  --filters "Name=tag:Name,Values=$vpc_name" \
+  --filters "Name=tag:Name,Values=$VPC_NAME" \
   --query 'Vpcs[0].VpcId' \
-  --output text)
-
-# Retrieve Security Group ID
-security_group_id=$(aws ec2 describe-security-groups \
-  --filters "Name=group-name,Values=$security_group_name" "Name=vpc-id,Values=$vpc_id" \
-  --query 'SecurityGroups[0].GroupId' \
   --output text)
 
 # Retrieve subnet IDs
@@ -25,25 +21,35 @@ subnet_ids=$(aws ec2 describe-subnets \
   --query 'Subnets[*].SubnetId' \
   --output text)
 
-# Retrieve ACM Certificate ARN
-certificate_arn=$(aws acm list-certificates \
-  --query "CertificateSummaryList[?DomainName=='$certificate_domain'].CertificateArn" \
-  --output text)
-
-# Retrieve Target Group ARN
-target_group_arn=$(aws elbv2 describe-target-groups \
-  --names "$target_group_name" \
-  --query 'TargetGroups[0].TargetGroupArn' \
+# Retrieve Security Group ID
+security_group_id=$(aws ec2 describe-security-groups \
+  --filters "Name=group-name,Values=$SECURITY_GROUP_NAME" "Name=vpc-id,Values=$vpc_id" \
+  --query 'SecurityGroups[0].GroupId' \
   --output text)
 
 # Create Application Load Balancer
 load_balancer_arn=$(aws elbv2 create-load-balancer \
-  --name "$load_balancer_name" \
+  --name "$LOAD_BALANCER_NAME" \
   --subnets $subnet_ids \
   --security-groups "$security_group_id" \
   --scheme internet-facing \
   --type application \
   --query 'LoadBalancers[0].LoadBalancerArn' \
+  --output text)
+
+echo "ALB has been configured."
+
+echo "Creating Listener with existing ACM and Target Group..."
+
+# Retrieve ACM Certificate ARN
+certificate_arn=$(aws acm list-certificates \
+  --query "CertificateSummaryList[?DomainName=='$CERTIFICATE_DOMAIN_NAME'].CertificateArn" \
+  --output text)
+
+# Retrieve Target Group ARN
+target_group_arn=$(aws elbv2 describe-target-groups \
+  --names "$TARGET_GROUP_NAME" \
+  --query 'TargetGroups[0].TargetGroupArn' \
   --output text)
 
 # Create HTTPS Listener with a forward action to the target group
@@ -55,6 +61,8 @@ listener_arn=$(aws elbv2 create-listener \
   --default-actions Type=forward,TargetGroupArn="$target_group_arn" \
   --query 'Listeners[0].ListenerArn' \
   --output text)
+
+echo "Listener has been configured."
 
 echo "Application Load Balancer with single target group has been configured. Create DNS CNAME:"
 aws elbv2 describe-load-balancers --load-balancer-arns "$load_balancer_arn" --query 'LoadBalancers[0].DNSName' --output text
